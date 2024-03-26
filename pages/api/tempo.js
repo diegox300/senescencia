b49a83ae52add6a752890befeeb148e51ea7e2c6
Diego Gervasio <diegox300@gmail.com>
2024-03-26 16:41:03 -0300

    Consumindo API do convertkit sem boas praticas de segurança

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
-function tempo (request, response) {
+async function tempo (request, response) {
     const dynamicDate = new Date();
 
-    response.json ({
-        date: dynamicDate.toGMTString()
+    const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=J2BF-fvV6e6U3luIDKktpM0Bw_wHC_jDg_bUekFLW00");
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
+        response.json ({
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     });
 }
 
